9c863f00bac0c4eca071906d2ff640da0005ee7a
r3pt1s <leonistmegacool2@gmail.com>
2025-12-31 21:28:57 +0100

    promoted properties exist since 2020

--- src/xxFLORII/bStats/charts/CallbackChart.php
+++ src/xxFLORII/bStats/charts/CallbackChart.php
@@ -4,27 +4,25 @@ declare(strict_types=1);
 namespace xxFLORII\bStats\charts;
 
 use Closure;
-use GlobalLogger;
+use pocketcloud\cloud\console\log\CloudLogger;
 use ReflectionClass;
 use Throwable;
 
 abstract class CallbackChart extends CustomChart {
 
-    protected $callback;
-
-    public function __construct(string $custom_id, Closure $callback) {
+    public function __construct(
+        string $custom_id,
+        protected readonly Closure $callback
+    ) {
         parent::__construct($custom_id);
-        $this->callback = $callback;
     }
 
     protected function call(): mixed {
         try {
             return ($this->callback)();
         } catch (Throwable $t) {
-            GlobalLogger::get()->error("Error while executing callback in " .
-                (new ReflectionClass($this))->getShortName() .
-                " class:");
-            GlobalLogger::get()->logException($t);
+            CloudLogger::get()->error("Error while executing callback in " . new ReflectionClass($this)->getShortName() . " class:");
+            CloudLogger::get()->exception($t);
             return null;
         }
     }
